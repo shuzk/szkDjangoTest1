a3e922d7b581e66378828fad87bc9959e262cbaa
test1 <test1@163.com>
2018-11-11 23:47:21 +0800

    test1_02choices选项和富文本编辑器

--- booktest/admin.py
+++ booktest/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from booktest.models import GoodsTest
 
 # Register your models here.
+
+admin.site.register(GoodsTest)
--- booktest/models.py
+++ booktest/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from tinymce.models import HTMLField
+
 
 # Create your models here.
+
+class GoodsTest(models.Model):
+    STATUS_CHOICES = (
+        (0, '下架'),
+        (1, '上架')
+    )
+    status = models.SmallIntegerField(choices=STATUS_CHOICES, verbose_name='商品状态')
+    detail = models.TextField()
+    class Meta:
+        db_table = 'df_goods_test'
+        verbose_name = "商品"
+        verbose_name_plural = verbose_name
